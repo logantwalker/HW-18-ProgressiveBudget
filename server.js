cb4dd4b0a8b7c5e016959a1e3ecef5bc9cb72693
L.Walker <logantwalker95@gmail.com>
2020-10-08 13:38:33 -0400

    formatting

--- server.js
+++ server.js
@@ -17,20 +17,16 @@ app.use(htmlRoutes);
 app.use(apiRoutes);
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://lwalker37:amv12v@cluster0.p7jgf.mongodb.net/Cluster0?retryWrites=true&w=majority',
-{   useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-})
-    .then(result =>{
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    })
+    .then(result => {
         app.listen(PORT)
         console.log('http://localhost:' + PORT);
     })
     .catch(err => {
         console.log(err);
     });
-
-// app.listen(PORT, function() {
-//     // Log (server-side) when our server has started
-//     console.log("Server listening on: http://localhost:" + PORT);
-//   });
