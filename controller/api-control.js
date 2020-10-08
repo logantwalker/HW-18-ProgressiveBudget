94a0e4062070bf7997c39b7f8e2fed570b409422
L.Walker <logantwalker95@gmail.com>
2020-10-08 13:56:55 -0400

    modularizing transaction post method

--- controller/api-control.js
+++ controller/api-control.js
@@ -1,7 +1,18 @@
+const path = require('path');
 const Transaction = require('../models/transaction');
 
 exports.fetchAll = (req, res, next) => {
     Transaction.find({}).sort({date: -1})
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(404).json(err);
+    });
+}
+
+exports.postTransaction = (req , res, next) => {
+  Transaction.create(req.body)
     .then(dbTransaction => {
       res.json(dbTransaction);
     })
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,22 +1,12 @@
 const express = require('express');
 
-const Transaction = require('../models/transaction');
-
 const apiControl = require('../controller/api-control');
 
 const router = express.Router();
 
 router.get("/api/transaction", apiControl.fetchAll);
 
-router.post("/api/transaction", ({ body }, res) => {
-    Transaction.create(body)
-        .then(dbTransaction => {
-            res.json(dbTransaction);
-        })
-        .catch(err => {
-            res.status(404).json(err);
-        });
-});
+router.post("/api/transaction",apiControl.postTransaction);
 
 
 module.exports = router;
